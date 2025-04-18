2e11123f0a66f3b2c53ac2ed64bb1f9f35a82c8d
SujalGarg2143 <sujal2143.be23@chitkara.edu.in>
2025-04-18 12:41:01 +0530

    added router level middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,11 @@ import { loginUser, signupUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    console.log(`Router level middle ware used on auth routes`);
+    next();
+})
+
 // Define Login & Signup Routes
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
